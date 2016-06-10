431b3364ddf047766a10f96dcf40acf912d2addc
Cosinity <accostenoble@gmail.com>
2016-06-10 17:03:30 -0400

    Implemented getMusic

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -3,8 +3,6 @@ package cs3500.music.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Arrays.asList;
-
 /**
  * Implements the functionality of the MusicPlayerModel interface
  */
@@ -21,20 +19,25 @@ public class MusicPlayer implements MusicPlayerModel {
 
   @Override
   public void write(Note n, int beat) {
-    if (beat < 1) {
+    if (beat < 0) {
       throw new IllegalArgumentException("Invalid beat");
     }
     if (lowNote == null || n.compareTo(lowNote) < 0) {
       lowNote = n;
     }
-    if (highNote == null || n.compareTo(highNote) > ) {
+    if (highNote == null || n.compareTo(highNote) > 0) {
       highNote = n;
     }
     while (beat + n.getDuration() > notes.size()) {
       notes.add(new ArrayList<Note>());
     }
-    for (int i = beat - 1; i < beat - 1 + n.getDuration(); i++) {
-      notes.get(i).add(n);
+    for (int i = beat; i < beat + n.getDuration(); i++) {
+      if (i == beat) {
+        notes.get(i).add(n);
+      } else {
+        // Use a note of length 0 to represent a continuation of a previous note
+        notes.get(i).add(new Note(n.getName(), n.getOctave(), 0));
+      }
     }
   }
 
@@ -50,31 +53,89 @@ public class MusicPlayer implements MusicPlayerModel {
 
   @Override
   public List<Note> getNotes(int beat) {
-    if (beat < 1) {
+    if (beat < 0) {
       throw new IllegalArgumentException("Invalid beat");
     }
-    return notes.get(beat - 1);
+    return notes.get(beat);
   }
 
   @Override
   public void remove(Note n, int beat) {
-    if (!notes.get(beat - 1).contains(n)) {
+    if (!notes.get(beat).contains(n)) {
       throw new IllegalArgumentException("Note does not exist at this beat");
     }
-    notes.get(beat - 1).remove(n);
+    Note cont = new Note(n.getName(), n.getOctave(), 0);
+    for (int i = beat; i < beat + n.getDuration(); i++) {
+      notes.get(i).remove(n);
+      notes.get(i).remove(cont);
+    }
   }
 
   @Override
   public void change(Note n1, int beat, Note n2) {
-    if (!notes.get(beat - 1).contains(n1)) {
+    if (!notes.get(beat).contains(n1)) {
       throw new IllegalArgumentException("Note does not exist at this beat");
     }
-    int nIdx = notes.get(beat - 1).indexOf(n1);
-    notes.get(beat - 1).set(nIdx, n2);
+    remove(n1, beat);
+    write(n2, beat);
   }
 
   @Override
-  public String getNotation() {
-    return null;
+  public String getMusic() {
+    if (notes.size() == 0) {
+      return "";
+    }
+    StringBuilder sb = new StringBuilder();
+    Note cur = lowNote;
+    Note oneAboveHigh = highNote.nextNote();
+    int beatDigits = String.valueOf(notes.size()).length();
+
+    /*
+     * Write the header
+     */
+    for (int i = 0; i < beatDigits; i++) {
+      sb.append(" ");
+    }
+    // Use compareTo instead of equals because it doesn't check duration
+    while (cur.compareTo(oneAboveHigh) != 0) {
+      String str = String.format("%5s", cur.toString() + " ");
+      sb.append(str);
+      cur = cur.nextNote();
+    }
+    sb.append("\n");
+
+    /*
+     * Write the notes and beat numbers
+     */
+    cur = lowNote;
+    for (int i = 0; i < notes.size(); i++) {
+      sb.append(String.format("%"+beatDigits+"d", i));
+      while (cur.compareTo(oneAboveHigh) != 0) {
+        boolean contains = false;
+        boolean beginning = false;
+        for (Note n : notes.get(i)) {
+          if (n.compareTo(cur) == 0) {
+            contains = true;
+            if (n.getDuration() != 0) {
+              beginning = true;
+            }
+          }
+        }
+        if (contains) {
+          if (beginning) {
+            sb.append("  X  ");
+          } else {
+            sb.append("  |  ");
+          }
+        } else {
+          sb.append("     ");
+        }
+        cur = cur.nextNote();
+      }
+      cur = lowNote;
+      sb.append("\n");
+    }
+
+    return sb.toString();
   }
 }
